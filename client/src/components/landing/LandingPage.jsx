0dda24a658082f163dd62588cfed8591208462e3
jo-ok22 <lunajosiasm@gmail.com>
2024-05-08 19:35:51 -0300

    actualización de Pokemon App

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -4,7 +4,7 @@ import style from './LandingPage.module.css';
 const LandingPage = () => {
     return(
         <div>
-            <h1 className={style.title}>Henry Pokemon</h1>
+            <h1 className={style.title}>Pokemon App</h1>
             <img src="fondo-blanco.png" alt="pokemon app" height="600px" style={{ width: '90%' }}/>
             <Link to='/home'>
             <button className={style.button}>Home</button>
